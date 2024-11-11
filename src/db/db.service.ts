cfda0a3b982a6e3523989a0504a6dc2ad32f124b
vigo44 <vvigo@rambler.ru>
2024-11-11 19:41:12 +0400

    feat: add albums db

--- src/db/db.service.ts
+++ src/db/db.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { UserDbService } from './users-db.service';
 import { ArtistDbService } from './artist-db.service';
 import { TrackDbService } from './track-db.service';
+import { AlbumDbService } from './album-db.service';
 
 @Injectable()
 export class DbService {
@@ -9,5 +10,6 @@ export class DbService {
     public readonly usersDbService: UserDbService,
     public readonly artistsDbService: ArtistDbService,
     public readonly tracksDbService: TrackDbService,
+    public readonly albumssDbService: AlbumDbService,
   ) {}
 }
