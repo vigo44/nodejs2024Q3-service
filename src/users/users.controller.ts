c5ce97722f8462e8372c313596e7e9b1087c9193
vigo44 <vvigo@rambler.ru>
2024-11-11 21:31:15 +0400

    fix: add validation uuid for patch into users controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -37,7 +37,10 @@ export class UsersController {
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+  update(
+    @Param('id', ParseUUIDPipe) id: string,
+    @Body() updateUserDto: UpdateUserDto,
+  ) {
     return this.usersService.update(id, updateUserDto);
   }
 
