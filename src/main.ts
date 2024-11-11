99d8c12c7f3661715655be47254d668b371da2c6
vigo44 <vvigo@rambler.ru>
2024-11-11 05:15:37 +0400

    feat: add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { readFile } from 'fs/promises';
+import { join } from 'path';
+import { SwaggerModule } from '@nestjs/swagger';
+import { parse } from 'yaml';
 
 const port = process.env.PORT ?? 4000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  const document = await readFile(
+    join(__dirname, '..', 'doc/api.yaml'),
+    'utf-8',
+  );
+  SwaggerModule.setup('doc', app, parse(document));
   await app.listen(port);
 }
 bootstrap();
